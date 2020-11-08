306764f886c44f61314df3b47f4b6864180caad4
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-11-07 23:51:42 -0800

    Created files and added js script

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1 -
+let personAccount = {
+  firstName : 'Kasym',
+  lastName : 'Jan',
+  incomes : [3000, 4000, 5000],
+  expenses : [3000, 3000, 1200, 50, 60],
+
+  //2
+  totalIncome(){
+
+  }
+
+  //3 
+  totalExpense(){
+
+  }
+
+  //4
+  addIncome(){
+
+  }
+
+  //5
+  addExpense(){
+
+  }
+
+  //6
+  accountInfo(){
+    
+  }
+
